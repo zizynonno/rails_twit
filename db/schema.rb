2269473884c31dfbe37e4b3d926980cfff3177ba
zizynonno <zizynonno@gmail.com>
2019-06-05 14:54:05 +0900

    RT6 finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190603231833) do
+ActiveRecord::Schema.define(version: 20190603233909) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
