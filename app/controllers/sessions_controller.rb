428703b07d3d2c6c546d5a08a63bfbeb8c49d2a4
zizynonno <zizynonno@gmail.com>
2019-06-06 23:04:54 +0900

    log in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    # userが存在していて(trueで)、パスワードが一致しているなら
+    if user && user.authenticate(params[:session][:password])
+      # 親クラスのApplicationContollerのinclude SessionsHelperのlog_inメソッドを使った
+      log_in user
+      # userページに飛ぶ
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
